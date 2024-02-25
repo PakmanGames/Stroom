b592cf7972ac1df8f3a14ed8bae191efb7dff899
Andy Pak <im.andy.pak@gmail.com>
2024-02-25 03:57:09 -0500

    Add logout feature

--- login_system/router.js
+++ login_system/router.js
@@ -10,11 +10,32 @@ const credentials = {
 router.post('/route/login', (req, res) => {
     if(req.body.email == credentials.email && req.body.password == credentials.password) {
         req.session.user = req.body.email;
-        res.redirect('/dashboard');
+        res.redirect('/route/dashboard');
         // res.end('Login Successful');
     } else {
         res.end('Invalid Password');
     }
+});
+
+// Route for dashboard
+router.get('/route/dashboard', (req, res) => {
+    if(req.session.user) {
+        res.render('dashboard', { user: req.session.user });
+    } else {
+        res.send('Unauthorized User');
+    }
+});
+
+// Route for Logout
+router.get('/route/logout', (req, res) => {
+    req.session.destroy(function(err) {
+        if(err) {
+            console.log(err);
+            res.send('Error');
+        } else {
+            res.render('base', { title: "Express", logout: "Logout Successful"});
+        }
+    });
 });
 
 module.exports = router;
